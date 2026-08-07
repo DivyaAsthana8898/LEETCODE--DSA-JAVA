331f4919ae537e64c97b831cc110c7583e60c96e
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-07 23:26:54 +0530

    Time: 1 ms (99.95%), Space: 47.2 MB (13.9%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xorSum = 0;
+        for(int n : nums){
+            xorSum = xorSum ^ n;
+
+        }
+          return xorSum;
+
+    }
+    
+}
