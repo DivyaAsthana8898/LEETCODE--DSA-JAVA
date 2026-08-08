f1364918f3e36d73150ba9b07c7dd1642e5fb1ec
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-09 01:59:26 +0530

    Time: 0 ms (100%), Space: 43.3 MB (97.31%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,34 +1,28 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int N = matrix.length;
-        // transposing
-        // with diagonal technique
-         
-        for(int i = 0;i<N ; i++){
-            for(int j = i+1;j<N;j++){
-                // now swaping;
-                int temp = matrix[i][j];
+             int N = matrix.length;
+        // transposing the array;
+        for(int i = 0 ;i < N ; i ++){
+            for (int j = i + 1; j<N ; j++){
+                int temp  = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-                // transposing been done;
+                matrix[j][i] =  temp ;
 
-            }// now reversing
-        }
-             for(int row = 0;row < N ; row++ ){
+
+            }
+        }// REVERSING 
+        for(int row = 0;row<N;row++){
             int startCol = 0;
             int endCol = N-1;
-            while(startCol <= endCol){// CONDITION
-                //reversing the first and the last column 
-            int temp = matrix[row][startCol];
-            matrix[row][startCol] = matrix[row][endCol];
-            matrix[row][endCol] = temp;
-            
-            startCol++;
-            endCol--;
+            while(startCol <= endCol){
+                int temp = matrix[row][startCol];
+                matrix[row][startCol] = matrix[row][endCol];
+                matrix[row][endCol] = temp;
 
-
-             }
-             
+                startCol++;
+                endCol--;
+                
+            }
         }
     }
-    }
+}
