a8a6d153ba89ff3793f44ca44c23e90955ff92c4
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-11 19:13:32 +0530

    Time: 0 ms (100%), Space: 44.6 MB (56.38%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+         int maxWealth = 0; 
+         for(int i = 0;i<accounts.length;i++){//for row which customer to add first ;
+         int sum = 0;
+           for(int j = 0; j < accounts[i].length;j++){//  which column bank needs to be process first;
+            sum = sum + accounts[i][j];
+           }
+           maxWealth = Math.max(maxWealth,sum);
+
+            
+         }
+         return maxWealth;
+        
+        
+    }
+}
