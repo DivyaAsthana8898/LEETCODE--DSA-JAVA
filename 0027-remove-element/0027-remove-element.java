5da88406c12c42c8043822022855b832bb81d3ac
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-12 00:53:50 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.73%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -2,7 +2,7 @@ class Solution {
     public int removeElement(int[] nums, int val) {
         int slow =0;
         int n = nums.length;
-        for(int fast=0;fast<n;fast++){
+        for(int fast=0;fast<n;fast++){ // fast = 0 Because the first element might be the value we're supposed to remove!
             if(nums[fast]!= val){
                 nums[slow] = nums[fast];
                 slow++;
@@ -10,6 +10,6 @@ class Solution {
             }
            
         }
-          return slow;
+          return slow;// return NUMBER OF VALID ELEMENTS
     }
 }
