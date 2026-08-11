d49efaaa063c288f78aaacb266ff5b1f250217a7
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-11 18:42:23 +0530

    Time: 1 ms (97.72%), Space: 47 MB (76.89%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+         int n = nums.length;
+         int []ans = new int [2*n];
+         for(int i=0;i<n;i++){
+           ans[i] = nums[i];// copy the first half;
+           ans[i + n] = nums[i]; // nums[i] = 0;   ans[i] = 0 as will start from 0 + n where  n is the length of prev array suppose 3; 0 + 3= 3 ans[3] = nums[0] put nums[0] index value at ans 3rd index. it is saying. 
+
+         }
+          return ans;
+
+    }
+}
