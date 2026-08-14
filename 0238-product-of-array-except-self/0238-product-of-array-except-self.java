a6c97c1962450fc5cc7e6fa7585a84cf82b7ebfa
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-14 15:24:55 +0530

    Time: 2 ms (93.89%), Space: 72.1 MB (29.07%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -2,17 +2,19 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int ans [] = new int [n];// creating new array
-         ans[0] = 1;
+         ans[0] = 1;//0 index set by 1
          for(int i = 1;i<n;i++){
             ans[i] = ans[i -1] * nums[i-1]; //2*3 for 3 in array = 6
 
          }
          int suffix = 1;
-         for(int i = n-1;i>=0;i--){
-         ans[i] = ans[i] * suffix;
-         suffix = suffix * nums[i]; 
+         for(int i = n-1;i>=0;i--){// // suffixes rhs product [24,24,12,4]
+
+         ans[i] = ans[i] * suffix;//// ans[i] = is the lhs arrays product and the suffix is the total sum of right product  6 * 1
+         suffix = suffix * nums[i]; // ye new suffix bnane k liye rhs product  ex right prod->1*4->el
+
          }
          return ans;
          }
          
-    }
+    }// 2361 14
