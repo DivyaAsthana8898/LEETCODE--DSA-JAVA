54049476f8f8055d1d880159ff0e9369c267dbe8
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-19 23:36:38 +0530

    Time: 0 ms (100%), Space: 47 MB (11.76%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.java
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -1,28 +1,20 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
-        //  condition if matrix is emptty or null
         if(matrix == null || matrix.length == 0){
             return new int[0][0];
-
-
         }
-         // original MATRIX
-         int totalRows = matrix.length;
-         int totalColumn = matrix[0].length;
+        int totalRows   = matrix.length;
+        int totalColumn = matrix[0].length;
 
-         // new MATRIX been built WITH THIS ORDER R=C C=R
-         int newTotalRows = totalColumn;
-         int newTotalColumn = totalRows;
-         int [][]ans = new int [newTotalRows][newTotalColumn];
-         // actual logic 
-                             // for traversing over prev matrix
-         for(int i = 0; i<totalRows;i++ ){
+        int newRows = totalColumn;
+        int newColumns = totalRows;
+
+        int [][]ans = new int [newRows][newColumns];
+        for(int i = 0;i<totalRows;i++){
             for(int j = 0;j<totalColumn;j++){
-
-                // to pour the values of old matrix into new in transpose manner;
                 ans[j][i] = matrix[i][j];
             }
-         }
-         return ans;
-    }
+        }
+        return ans;
+    } 
 }
