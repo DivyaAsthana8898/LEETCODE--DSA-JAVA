116a8cb0591c5aa8dbdce7a7e0e4ce0e9f68848f
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-20 16:28:56 +0530

    Time: 2 ms (30.39%), Space: 44.7 MB (32.74%) - LeetHub

---
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            int count = 0;
+            while(nums[i] != 0){
+           
+             nums[i] = nums[i] / 10;// counting the digits
+              count ++;
+            }
+                if(count % 2 == 0){
+                    ans++;
+                 
+                    
+
+                }
+            }
+               return ans;
+        }
+     
+    }
+
+// count → current number ki digits
+
+// ans → poore array mein kitne valid numbers
