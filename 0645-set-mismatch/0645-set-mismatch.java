66e9d8a780276d0a29547648f7b3cbe827fe048c
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-20 19:14:30 +0530

    Time: 14 ms (31.87%), Space: 48.6 MB (6.68%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,30 +1,24 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-
+        int n = nums.length;
         Arrays.sort(nums);
-
+        // duplicate
         int duplicate = -1;
-
-        // Find duplicate
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
+        for(int i = 1;i<n;i++){
+            if(nums[i] == nums[i-1]){
                 duplicate = nums[i];
                 break;
             }
         }
-
-        int n = nums.length;
-
-        int expectedSum = n * (n + 1) / 2;
-
+        // missing
+  
+        int expectedSum = n * (n+1) / 2;
         int actualSum = 0;
+        for(int num : nums){
+         actualSum += num;
+    }
+      int missing = expectedSum - actualSum + duplicate;
+      return new int[]{duplicate , missing};
 
-        for (int num : nums) {
-            actualSum += num;
-        }
-
-        int missing = expectedSum - actualSum + duplicate;
-
-        return new int[]{duplicate, missing};
     }
 }
