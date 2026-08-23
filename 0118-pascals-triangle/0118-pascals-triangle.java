49bc2a4e4ceb9602adfa4059747a64f9db2d8b64
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-23 23:37:42 +0530

    Time: 1 ms (95.44%), Space: 43.3 MB (71.62%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -6,11 +6,11 @@ class Solution {
         for(int i = 0;i<numRows;i++){
             List<Integer> row = new ArrayList<>();
             for(int j = 0 ;j<=i;j++){
-                // handling 1st and last case
+                // handling 1st and last indexes
                 if(j == 0 || j==i){
                     row.add(1);
                 }
-                else{
+                else{ // handling middle indexes
                     int value = ans.get(i-1).get(j-1)+ans.get(i-1).get(j);
                     row.add(value);
                 }
