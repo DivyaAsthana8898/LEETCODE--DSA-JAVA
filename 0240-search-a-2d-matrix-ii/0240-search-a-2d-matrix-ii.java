c0f2ebed05e1cd0560d5a2fc177f5a481ddb94be
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-24 20:45:47 +0530

    Time: 3 ms (87.76%), Space: 47.9 MB (96.56%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -9,9 +9,9 @@ class Solution {
         int col = totalColumn-1;//col pointer starting from the end pos;
 
         while(row < totalRow && col >= 0){
-            if(matrix[row][col] == target){
+            if(matrix[row][col] == target)
                 return true;
-            }
+            
             else if(matrix[row][col] > target){
                 col--;
             }else{
