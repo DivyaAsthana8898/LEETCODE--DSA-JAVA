b13c147fc9cbb72603e6712797f0b319dd46d5fe
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-28 01:10:43 +0530

    Time: 9 ms (98.91%), Space: 50.1 MB (76.48%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,43 +1,37 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-
         int start = 0;
         int end = 0;
+/// limit of capacity must not breach
+// likewise the days must also be <= days
+        for(int weight : weights){
+            start = Math.max(start,weight);// maximum possible package weight
+            end = end + weight;// maximum possible capacity ;
 
-        // Find minimum and maximum possible capacity
-        for (int weight : weights) {
-            start = Math.max(start, weight);
-            end += weight;
         }
-
-        // Binary Search on answer
-        while (start < end) {
-
-            int mid = start + (end - start) / 2;
-
-            int day = 1;
-            int currentWeight = 0;
-
-            for (int weight : weights) {
-
-                if (currentWeight + weight > mid) {
-                    day++;
-                    currentWeight = 0;
-                }
-
-                currentWeight += weight;
+             // Binary Search
+         while(start< end ){
+            int mid = start + (end - start)/2;
+             int day = 1;
+             int currentWeight = 0;
+             for(int weight : weights){
+            if(currentWeight + weight > mid){
+                day++;
+                currentWeight=0;
             }
-
-            // Capacity mid is enough
-            if (day <= days) {
+            currentWeight += weight;
+             }
+               // 2<=5 jyada le ja rhe ho aur kam ja skta h load
+              if(day <= days){
                 end = mid;
-            }
-            // Capacity mid is not enough
-            else {
+              }
+              // bhut kam le ja rhe ho capacity ship ki bdhyo 6<=5 false condition
+              else{
                 start = mid + 1;
-            }
-        }
-
-        return start;
+              }
+             
+         }
+         return end;
+         
     }
 }
