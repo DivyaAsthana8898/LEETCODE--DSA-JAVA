82346552c8a7f962ada4cd4a37f7a352594d274d
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-29 17:15:54 +0530

    Time: 0 ms (100%), Space: 46.3 MB (83.91%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit= 0;
+        int profit = 0;
         int prevProfit = 0;
-        for(int i=1;i<prices.length;i++){
-            if(prices[i]>prices[i-1]){
+        for(int i = 1;i<prices.length;i++){
+            if(prices[i] > prices[i-1]){
                 prevProfit = prices[i] - prices[i-1];
                 profit = profit + prevProfit;
+                
             }
-        } 
-         return profit;
-
-      }
-  }
- 
+        }
+        return profit;
+    }
+}
+//prices[i] is selling prices prices[i-1] is cp
