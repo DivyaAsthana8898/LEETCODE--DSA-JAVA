97694390aa35d01a31d6e977484de8e944a966f8
Divya Asthana <divyakumar12a48@gmail.com>
2026-08-30 22:15:24 +0530

    Time: 2 ms (99.34%), Space: 47.1 MB (59.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,32 +1,32 @@
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//         HashMap<Integer,Integer> map = new HashMap<>();
-//                int n = nums.length;
-//         for(int i = 0;i<n;i++){
-
-//             int needed = target - nums[i];
-//             while(map.containsKey(needed)){
-//                 return new int[]{map.get(needed),i};
-//             }
-//             map.put(nums[i] , i);
-
-//         }
-//         return new int[]{};
-//     }
-// }
-// brute force 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i =0;i< nums.length;i++){
-            for(int j= i+1 ; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    int [] ans = {i,j};
-                    return ans;
-                }
+        HashMap<Integer,Integer> map = new HashMap<>();
+               int n = nums.length;
+        for(int i = 0;i<n;i++){
 
+            int needed = target - nums[i];
+            while(map.containsKey(needed)){
+                return new int[]{map.get(needed),i};
             }
+            map.put(nums[i] , i);
+
         }
-         int ans[] = {};
-         return ans;
+        return new int[]{};
     }
 }
+//brute force 
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         for(int i =0;i< nums.length;i++){
+//             for(int j= i+1 ; j < nums.length; j++){
+//                 if(nums[i] + nums[j] == target){
+//                     int [] ans = {i,j};
+//                     return ans;
+//                 }
+
+//             }
+//         }
+//          int ans[] = {};
+//          return ans;
+//     }
+// }
