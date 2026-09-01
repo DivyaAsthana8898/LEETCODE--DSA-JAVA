ca60c746436c37823faa24d460cf6f158063c376
Divya Asthana <divyakumar12a48@gmail.com>
2026-09-01 23:53:53 +0530

    Time: 15 ms (24.14%), Space: 46.8 MB (9.11%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        for(char ch: s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0) + 1);
+                       // generating map key => value
+            }
+        
+        for(char ch: t.toCharArray()){
+            if(!map.containsKey(ch) || map.get(ch)==0){
+                return false;
+            }
+            map.put(ch,map.get(ch)-1);// freq downing
+        }
+        return true;
+    }
+}
