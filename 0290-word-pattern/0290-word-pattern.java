0026d4d20597b30a7b0b9144310cbac5b4a722d9
Divya Asthana <divyakumar12a48@gmail.com>
2026-09-03 00:44:38 +0530

    Time: 1 ms (84.04%), Space: 43.1 MB (5.57%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -5,17 +5,10 @@ class Solution {
          String[] str1 = s.split(" ");
            if(pattern.length() != str1.length) {
                       return false;
-                    }
-          
-
+                    }// check Kyuki pattern ke characters aur s ke words ki count same honi chahiye
         for(int i = 0;i<pattern.length();i++){
-           
                   char ch = pattern.charAt(i);
                   String str = str1[i];
-
-                 
-                 
-        
         if(map.containsKey(ch) && !map.get(ch).equals(str)){
          return false;
         }
