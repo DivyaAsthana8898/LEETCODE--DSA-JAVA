f0a0308b06d1fbda3d84b1931cd1a574fa23736c
Divya Asthana <divyakumar12a48@gmail.com>
2026-09-03 00:39:31 +0530

    Time: 1 ms (84.04%), Space: 42.8 MB (55.88%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character,String> map = new HashMap<>();
+        HashMap<String,Character> map2 = new HashMap<>();
+         String[] str1 = s.split(" ");
+           if(pattern.length() != str1.length) {
+                      return false;
+                    }
+          
+
+        for(int i = 0;i<pattern.length();i++){
+           
+                  char ch = pattern.charAt(i);
+                  String str = str1[i];
+
+                 
+                 
+        
+        if(map.containsKey(ch) && !map.get(ch).equals(str)){
+         return false;
+        }
+          if(map2.containsKey(str) && map2.get(str) !=ch){
+         return false;
+        }
+       
+        map.put(ch,str);
+        map2.put(str,ch);
+        }
+
+        return true;
+
+
+    }
+
+}
