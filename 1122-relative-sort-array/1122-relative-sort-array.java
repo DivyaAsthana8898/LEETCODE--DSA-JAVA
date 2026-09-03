efad2c86dfdd731da849abcf39f14b3601f3d183
Divya Asthana <divyakumar12a48@gmail.com>
2026-09-04 01:22:45 +0530

    Time: 3 ms (43.33%), Space: 43.8 MB (42.42%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,34 +1,42 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        // frequency of arr1
-        for (int num : arr1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : arr1){
+        map.put(num,map.getOrDefault(num,0)+1);
         }
+         int index = 0;
+         for(int num :arr2){
+          if(map.containsKey(num)){////////// present   for one matching
+            while(map.get(num)>0){// if freq > 0
+            arr1[index] = num; // put the val in arr1
+            index++;// move ;
+            
+            map.put(num,map.get(num)-1);// freq kam kro;
 
-        int index = 0;
+          }
+          map.remove(num);
+           }
+         }
+           // for remaning not matching values 
+          for(int num = 0;num<= 1000;num++){
+            if(map.containsKey(num)){
+               while( map.get(num)>0){//// till the frequency is >0
+                  arr1[index] = num; //// put the  value in arr1
+                  index++;// move index ahead;
+                  map.put(num,map.get(num)-1);//frequency kam kro ;
+               }
 
-        // arr2 ke order me
-        for (int num : arr2) {
-            while (map.get(num) > 0) {
-                arr1[index++] = num;
-                map.put(num, map.get(num) - 1);
             }
-            map.remove(num);
-        }
-
-        // remaining elements ascending order
-        for (int num = 0; num <= 1000; num++) {
-            if (map.containsKey(num)) {
-                while (map.get(num) > 0) {
-                    arr1[index++] = num;
-                    map.put(num, map.get(num) - 1);
-                }
-            }
-        }
-
-        return arr1;
-    }
+        
+          }
+         return arr1; 
+     }   
 }
+  
+    
+
+
+
+
+
+
