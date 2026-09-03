077b81d36c3923957b75cecc120a1e9159d33319
Divya Asthana <divyakumar12a48@gmail.com>
2026-09-03 17:25:16 +0530

    Time: 1 ms (86.79%), Space: 44.3 MB (20.29%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,27 @@
+class Solution {
+    static void solve(int [] nums,int index, List<Integer> output,List<List<Integer>> ans){
+        if(index >= nums.length){
+           // base case
+           ans.add(new ArrayList<>(output));
+           return ;
+
+        }
+        int currValue = nums[index];
+        output.add(currValue);
+        //include 
+        solve(nums,index+1,output,ans);
+        //
+        output.remove(output.size()-1);
+        // exclude
+        solve(nums,index+1,output,ans);
+
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        int index = 0;
+         solve(nums,index,output,ans);
+        return ans;
+
+    }
+}
