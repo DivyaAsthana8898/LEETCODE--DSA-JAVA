a7856115bad7bd171f8fd6d5ab97c5d7003c0235
Divya Asthana <divyakumar12a48@gmail.com>
2026-09-03 22:27:52 +0530

    Time: 4 ms (6.05%), Space: 42.9 MB (71.88%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -7,12 +7,12 @@ class Solution {
         while (i < nums.length) {
             int start = nums[i];
 
-            while (i + 1 < nums.length && nums[i + 1] == nums[i] + 1) {
-                i++;// if current k age wala is bigger then current
+            while (i + 1 < nums.length && nums[i + 1] == nums[i] + 1) {//kya next number current number se exactly 1 bada hai
+                i++;
             }
 
             if (start == nums[i]) {
-                ans.add(String.valueOf(start));// same range me rkho 
+                ans.add(String.valueOf(start));// only one no  no range  
             } else {
                 ans.add(start + "->" + nums[i]);// range khatam
             }
