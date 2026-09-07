acbe51391fd003ec727c291bb4c2bf609ac1540a
Divya Asthana <divyakumar12a48@gmail.com>
2026-09-07 15:42:57 +0530

    Time: 1 ms (100%), Space: 42.8 MB (25.16%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -4,8 +4,8 @@ class Solution {
         long end = n;
 
         while(start <= end){
-            long mid = start + (end - start )/2;
-            long coins = mid * (mid+1)/2;
+            long mid = start + (end - start )/2;//mid representing no of rows
+            long coins = mid * (mid+1)/2;// formula for calculating total no of coins required 
             // check if coins are 6 == 6
             if(coins == n){
                 return (int) mid;
