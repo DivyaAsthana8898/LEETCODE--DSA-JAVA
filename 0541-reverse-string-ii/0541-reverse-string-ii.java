dbdc928bd308d03c0e6b0ce129d056d9da504174
Divya Asthana <divyakumar12a48@gmail.com>
2026-09-12 17:47:35 +0530

    Time: 1 ms (95.82%), Space: 45 MB (12.92%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.java
+++ 0541-reverse-string-ii/0541-reverse-string-ii.java
@@ -1,10 +1,12 @@
 class Solution {
     public String reverseStr(String s, int k) {
         char []word = s.toCharArray();
-        for(int start = 0;start<word.length;start += 2*k){//jump
+        for(int start = 0;start<word.length;start += 2*k){//start += 2*k;
+
+  // next 2k block par jump
 
             int left = start;
-            int right = Math.min(start+k-1,word.length-1);// right pos
+            int right = Math.min(start+k-1,word.length-1);//first k characters ka last index,
 
             while(left < right){
                 char  temp = word[left];
