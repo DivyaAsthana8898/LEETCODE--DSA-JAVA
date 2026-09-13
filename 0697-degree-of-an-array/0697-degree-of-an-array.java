d092c5517e404a0a17233834380f9ef2dc08fd3a
Divya Asthana <divyakumar12a48@gmail.com>
2026-09-13 23:52:23 +0530

    Time: 27 ms (46.37%), Space: 50.2 MB (50.76%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -1,45 +1,31 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        HashMap<Integer,Integer> first = new HashMap<>();
+        HashMap<Integer,Integer> last = new  HashMap<>();
 
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        HashMap<Integer, Integer> first = new HashMap<>();
-        HashMap<Integer, Integer> last = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);// stores the key as well as the frequency;
 
-        // frequency + first index + last index
-        for (int i = 0; i < nums.length; i++) {
-
-            // frequency
-            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
-
-            // first index — sirf first time
-            if (!first.containsKey(nums[i])) {
-                first.put(nums[i], i);
+            if(!first.containsKey(nums[i])){
+                first.put(nums[i],i);// stores only the key and its index
+                }
+                last.put(nums[i],i);// stores only the key and its index
             }
+            // now degree
+            int degree = 0;
+            for(int key : freq.values()){
+            degree = Math.max(degree,key);
 
-            // last index — har baar update
-            last.put(nums[i], i);
-        }
-
-        // degree find karo
-        int degree = 0;
-
-        for (int value : freq.values()) {
-            degree = Math.max(degree, value);
-        }
-
-        // degree wale elements ka shortest subarray
-        int ans = nums.length;
-
-        for (int key : freq.keySet()) {
-
-            if (freq.get(key) == degree) {
-
-                int length = last.get(key) - first.get(key) + 1;
-
-                ans = Math.min(ans, length);
             }
-        }
-
+            // now shortest subarray
+            int ans = nums.length;
+            for(int key : freq.keySet()){
+                if(freq.get(key) == degree){
+                    int length = last.get(key) - first.get(key) + 1;
+                    ans = Math.min(ans,length);
+                }
+            }   
         return ans;
     }
 }
