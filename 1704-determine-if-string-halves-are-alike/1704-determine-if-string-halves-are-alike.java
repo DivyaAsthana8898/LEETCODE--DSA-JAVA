394976402c995aacc75331e2198882b9b824b670
Divya Asthana <divyakumar12a48@gmail.com>
2026-09-17 00:47:04 +0530

    Time: 4 ms (55.24%), Space: 42.9 MB (86.04%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -4,31 +4,30 @@ class Solution {
         int a = 0;
         int b = n;
 
+        int count = 0;
+        int count1 = 0;
+
+        while(a < n){
+           
+            char ch = s.charAt(a);
+            if( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o'|| ch == 'u'||ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+            count++;
+        }
+        a++;
+    }
         
-          int count  = 0;
-          int count1 = 0;
-
-        while( a <  n){
-               char ch  = s.charAt(a);
-                   if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
-                   ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ){
-                   count++;
-            }
-                a++;
+        while( b < s.length()){
+           
+            char ch = s.charAt(b);
+            if( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o'|| ch == 'u'||ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+            count1++;
         }
+        
+        b++;
+    }
     
-            while( b < s.length()){
-               char ch  = s.charAt(b);
-                   if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
-                   ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ){
-                   count1++;
-            }
-                b++;
-            }
-    
-             return count == count1;
-
-        }
+    return count == count1;
 
 
     }
+}
